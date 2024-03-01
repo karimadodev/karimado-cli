1ff3565348c4daa058adf7759538792b82b8e957
John Doe <johndoe@example.com>
2024-03-01 23:15:16 +0800

    feat: Add `--verbose` flag

--- karimado/src/cli.rs
+++ karimado/src/cli.rs
@@ -3,8 +3,8 @@ mod new;
 mod run;
 mod scaffold;
 
-use anyhow::Result;
 use clap::{Parser, Subcommand};
+use clap_verbosity_flag::{InfoLevel, Verbosity};
 
 use crate::contrib;
 
@@ -13,6 +13,9 @@ use crate::contrib;
 struct Cli {
     #[command(subcommand)]
     command: Commands,
+
+    #[command(flatten)]
+    verbose: Verbosity<InfoLevel>,
 }
 
 #[derive(Subcommand)]
@@ -31,12 +34,32 @@ enum Commands {
     ScaffoldInstall(scaffold::InstallCommand),
 }
 
-pub fn execute() -> Result<()> {
+pub fn execute() -> i32 {
     let cli = Cli::parse();
-    match &cli.command {
+
+    let level_filter = cli.verbose.log_level_filter();
+    let timestamp: Option<env_logger::fmt::TimestampPrecision> =
+        if level_filter <= log::LevelFilter::Info {
+            None
+        } else {
+            Some(env_logger::fmt::TimestampPrecision::Seconds)
+        };
+    env_logger::Builder::new()
+        .format_level(false)
+        .format_target(false)
+        .format_timestamp(timestamp)
+        .filter_level(level_filter)
+        .init();
+
+    if let Err(r) = match &cli.command {
         Commands::New(cmd) => cmd.execute(),
         Commands::Run(cmd) => cmd.execute(),
         Commands::Build(cmd) => cmd.execute(),
         Commands::ScaffoldInstall(cmd) => cmd.execute(),
+    } {
+        log::error!("{:?}", r);
+        1
+    } else {
+        0
     }
 }
--- karimado/src/main.rs
+++ karimado/src/main.rs
@@ -1,9 +1,3 @@
 fn main() {
-    std::process::exit(match karimado::cli::execute() {
-        Ok(_) => 0,
-        Err(err) => {
-            eprintln!("{:?}", err);
-            1
-        }
-    });
+    std::process::exit(karimado::cli::execute())
 }

31d1ac2afb2fc5b8f806571bec31ea673e0f842f
John Doe <johndoe@example.com>
2024-03-07 01:45:22 +0800

    feat: Set up proxy using HTTPS_PROXY

--- karimado/src/core/download/git_https.rs
+++ karimado/src/core/download/git_https.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
 use fastrand::alphanumeric;
-use git2::Repository;
+use git2::{build::RepoBuilder, FetchOptions, ProxyOptions, Repository};
 use std::{
-    iter,
+    env, iter,
     path::{Path, PathBuf},
 };
 use url::Url;
@@ -13,7 +13,7 @@ pub(crate) fn download(url: &Url, downloads_path: &Path) -> Result<PathBuf> {
 
     let repo_name: String = iter::repeat_with(alphanumeric).take(8).collect();
     let repo_path = downloads_path.join(repo_name);
-    let repo = Repository::clone(&repo_url, &repo_path)?;
+    let repo = git_clone_repoistory(&repo_url, &repo_path)?;
 
     if let Some(committish) = url.fragment() {
         let (object, reference) = repo.revparse_ext(committish)?;
@@ -26,4 +26,29 @@ pub(crate) fn download(url: &Url, downloads_path: &Path) -> Result<PathBuf> {
     }
 
     Ok(repo_path)
+}
+
+fn git_clone_repoistory(repo_url: &str, repo_path: &Path) -> Result<Repository> {
+    let mut builder = RepoBuilder::new();
+    let mut builder = match env::var("HTTPS_PROXY") {
+        Ok(value) => {
+            log::debug!(
+                "set up proxy using environment variable HTTPS_PROXY={}",
+                value
+            );
+
+            let mut proxy_options = ProxyOptions::new();
+            proxy_options.url(&value);
+
+            let mut fetch_options = FetchOptions::new();
+            fetch_options.proxy_options(proxy_options);
+
+            builder.fetch_options(fetch_options);
+            builder
+        }
+        _ => builder,
+    };
+
+    log::debug!("cloning new repository: {}", repo_url);
+    Ok(builder.clone(repo_url, repo_path)?)
 }
--- karimado/src/core/download/https.rs
+++ karimado/src/core/download/https.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use fastrand::alphanumeric;
 use std::{
+    env,
     fs::{self, File},
     io, iter,
     path::{Path, PathBuf},
@@ -11,7 +12,7 @@ use crate::core::archive;
 
 pub(crate) fn download(url: &Url, downloads_path: &Path) -> Result<PathBuf> {
     let archive_url = String::from(url.clone());
-    let mut response = reqwest::blocking::get(archive_url)?;
+    let mut response = https_get(&archive_url)?;
 
     let archive_name: String = iter::repeat_with(alphanumeric).take(8).collect();
     let archive_path = downloads_path.join(archive_name.clone() + ".download");
@@ -23,4 +24,20 @@ pub(crate) fn download(url: &Url, downloads_path: &Path) -> Result<PathBuf> {
     fs::remove_file(&archive_path)?;
 
     Ok(target_path)
+}
+
+fn https_get(url: &str) -> Result<reqwest::blocking::Response> {
+    let builder = reqwest::blocking::Client::builder();
+    let builder = match env::var("HTTPS_PROXY") {
+        Ok(value) => {
+            log::debug!(
+                "set up proxy using environment variable HTTPS_PROXY={}",
+                value
+            );
+            builder.proxy(reqwest::Proxy::https(value)?)
+        }
+        Err(..) => builder.no_proxy(),
+    };
+
+    Ok(builder.build()?.get(url).send()?)
 }

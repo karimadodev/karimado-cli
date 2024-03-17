e3b77c3fc366eb4c812589a392a9eb551c5bf6fe
John Doe <johndoe@example.com>
2024-03-18 04:40:28 +0800

    test: Add more taskmgr tests

--- karimado-tasks/src/taskmgr/parallel_test.rs
+++ karimado-tasks/src/taskmgr/parallel_test.rs
@@ -1,17 +1,29 @@
+use std::time::{Duration, Instant};
+
 use super::*;
 use crate::build_task;
 
 #[test]
 fn ok() {
-    let tasks: Vec<Task> = vec![build_task!(command: "cargo version")];
+    let tasks: Vec<Task> = vec![
+        build_task!(command: "ruby -e 'sleep(1)'"),
+        build_task!(command: "ruby -e 'sleep(1)'"),
+    ];
 
+    let now = Instant::now();
     let r = execute(&tasks, false);
+    let elapsed = now.elapsed();
+
     assert!(r.is_ok());
+    assert!(elapsed < Duration::from_secs(2));
 }
 
 #[test]
 fn err_command_not_found() {
-    let tasks: Vec<Task> = vec![build_task!(command: "command404")];
+    let tasks: Vec<Task> = vec![
+        build_task!(command: "command404"),
+        build_task!(command: "ruby -e 'sleep(1)'"),
+    ];
 
     let r = execute(&tasks, false);
     assert!(r.is_err());
@@ -20,4 +32,20 @@ fn err_command_not_found() {
     let e = r.unwrap_err().to_string();
     assert!(e.clone().contains("failed to run task"));
     assert!(e.clone().contains("exited with code"));
+}
+
+#[test]
+fn err_exit_code_is_nonzero() {
+    let tasks: Vec<Task> = vec![
+        build_task!(command: "ruby -e 'exit(77)'"),
+        build_task!(command: "ruby -e 'sleep(1)'"),
+    ];
+
+    let r = execute(&tasks, false);
+    assert!(r.is_err());
+    assert!(matches!(r, Err(Error::TaskRunFailed(_))));
+
+    let e = r.unwrap_err().to_string();
+    assert!(e.clone().contains("failed to run task"));
+    assert!(e.clone().contains("exited with code 77"));
 }

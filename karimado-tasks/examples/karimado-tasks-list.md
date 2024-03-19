06691c70e76b5a95a8b3d60283f87324a9685aa3
John Doe <johndoe@example.com>
2024-03-19 19:22:26 +0800

    test: Add examples/karimado-tasks-list.md

---
+++ karimado-tasks/examples/karimado-tasks-list.md
@@ -0,0 +1,9 @@
+# taskmgr#list
+
+```console
+$ karimado-tasks-list
+Available tasks for this project:
+* ruby:false # Do nothing, unsuccessfully
+* ruby:true  # Do nothing, successfully
+
+```
---
+++ karimado-tasks/examples/karimado-tasks-list.rs
@@ -0,0 +1,17 @@
+use karimado_tasks;
+
+fn main() {
+    env_logger::builder()
+        .format_level(false)
+        .format_target(false)
+        .format_timestamp(None)
+        .filter_level(log::LevelFilter::Info)
+        .init();
+
+    let taskmgr = karimado_tasks::TaskMgr::builder()
+        .current_dir(&std::env::current_dir().unwrap())
+        .taskfile("examples/karimado/tasks.toml")
+        .build()
+        .unwrap();
+    taskmgr.list();
+}

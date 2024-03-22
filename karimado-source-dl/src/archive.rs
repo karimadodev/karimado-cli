a7e6567baa467ed085fe621eab3d456ad6b8db00
John Doe <johndoe@example.com>
2024-03-22 23:03:08 +0800

    feat: Add karimado_source_dl::archive::gzip

--- karimado-source-dl/src/archive.rs
+++ karimado-source-dl/src/archive.rs
@@ -1,15 +1,39 @@
-mod mime;
+mod gzip;
 mod zip;
 
 use std::path::Path;
+use std::str::FromStr;
+use strum::{Display, EnumIter, EnumString, IntoEnumIterator};
 
 use crate::error::*;
-use SourceDecompressErrorKind::IoError;
+use SourceDecompressErrorKind::{IoError, UnknownMimeType};
+
+#[derive(Display, EnumIter, EnumString)]
+pub(crate) enum DecompressType {
+    #[strum(serialize = "application/gzip")]
+    Gzip,
+    #[strum(serialize = "application/zip")]
+    Zip,
+}
+
+impl DecompressType {
+    pub(crate) fn parse(str: &str) -> Result<Self> {
+        DecompressType::from_str(str).map_err(|_| {
+            let v = Vec::from_iter(DecompressType::iter().map(|v| v.to_string()));
+            let e = format!(
+                "the decomporess type was expected one of {:?} but got {:?}",
+                v, str
+            );
+            Error::SourceDecompressError(UnknownMimeType(e))
+        })
+    }
+}
 
 pub(crate) fn decompress(archive_path: &Path, target_path: &Path) -> Result<()> {
     let kind = infer::get_from_path(archive_path).map_err(IoError)?;
     let kind = kind.map(|k| k.mime_type()).unwrap_or("unknown");
-    match mime::DecompressType::parse(kind)? {
-        mime::DecompressType::Zip => zip::decompress(archive_path, target_path),
+    match DecompressType::parse(kind)? {
+        DecompressType::Gzip => gzip::decompress(archive_path, target_path),
+        DecompressType::Zip => zip::decompress(archive_path, target_path),
     }
 }
---
+++ karimado-source-dl/src/archive/gzip.rs
@@ -0,0 +1,15 @@
+use flate2::read::GzDecoder;
+use std::fs::File;
+use std::path::Path;
+use tar::Archive;
+
+use crate::error::*;
+use SourceDecompressErrorKind::IoError;
+
+pub(crate) fn decompress(archive_path: &Path, target_path: &Path) -> Result<()> {
+    let file = File::open(archive_path).map_err(IoError)?;
+    let decoder = GzDecoder::new(file);
+    let mut archive = Archive::new(decoder);
+    archive.unpack(target_path).map_err(IoError)?;
+    Ok(())
+}
